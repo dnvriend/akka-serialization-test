e8e15d30fa07045da5aaf6f76edb317ebbc6c356
Dennis Vriend <dnvriend@gmail.com>
2016-06-18 07:21:55 +0200

    v1 to v5

--- src/test/scala/com/github/dnvriend/serializer/avro4s/AvroSENoOldClassesTest.scala
+++ src/test/scala/com/github/dnvriend/serializer/avro4s/AvroSENoOldClassesTest.scala
@@ -16,99 +16,101 @@
 
 package com.github.dnvriend.serializer.avro4s
 
+/**
+ * https://avro.apache.org/docs/1.7.6/spec.html
+ * https://docs.oracle.com/cd/NOSQL/html/GettingStartedGuide/schemaevolution.html
+ * https://martin.kleppmann.com/2012/12/05/schema-evolution-in-avro-protocol-buffers-thrift.html
+ * http://ben-tech.blogspot.nl/2013/05/avro-schema-evolution.html
+ */
 class AvroSENoOldClassesTest extends AvroTestSpec {
-
   "String" should "encode to base64" in {
-    val encoder = implicitly[Encoder[Array[Byte], String]]
-    val base64String = encoder.encode("Hello World!".getBytes())
-    base64String shouldBe "SGVsbG8gV29ybGQh"
+    "Hello World!".getBytes().encode[Base64].value shouldBe "SGVsbG8gV29ybGQh"
   }
 
   it should "decode from base64" in {
-    val decoder = implicitly[Decoder[String, Array[Byte]]]
-    new String(decoder.decode("SGVsbG8gV29ybGQh")) shouldBe "Hello World!"
+    Base64("SGVsbG8gV29ybGQh").decode[String] shouldBe "Hello World!"
   }
 
   "MovieChangedV1" should "encode to base64" in {
     val event = MovieChangedV1("foo", 1990)
-    val encoder = implicitly[Encoder[MovieChangedV1, String]]
-    val base64String = encoder.encode(event)
-    base64String shouldBe "BmZvb4wf"
+    val encoder = implicitly[Encoder[MovieChangedV1, Base64]]
+    val base64 = encoder.encode(event)
+    base64.value shouldBe "BmZvb4wf"
   }
 
   it should "decode from base64" in {
-    val decoder = implicitly[Decoder[String, MovieChangedV1]]
-    decoder.decode("BmZvb4wf") shouldBe MovieChangedV1("foo", 1990)
+    val decoder = implicitly[Decoder[Base64, MovieChangedV1]]
+    decoder.decode(Base64("BmZvb4wf")) shouldBe MovieChangedV1("foo", 1990)
   }
 
   "MovieChangedV2" should "encode to base64" in {
     val event = MovieChangedV2("foo", 1990, "bar")
-    val encoder = implicitly[Encoder[MovieChangedV2, String]]
-    val base64String = encoder.encode(event)
-    base64String shouldBe "BmZvb4wfBmJhcg=="
+    val encoder = implicitly[Encoder[MovieChangedV2, Base64]]
+    val base64 = encoder.encode(event)
+    base64.value shouldBe "BmZvb4wfBmJhcg=="
   }
 
   it should "decode from base64" in {
-    val decoder = implicitly[Decoder[String, MovieChangedV2]]
-    decoder.decode("BmZvb4wfBmJhcg==") shouldBe MovieChangedV2("foo", 1990, "bar")
+    val decoder = implicitly[Decoder[Base64, MovieChangedV2]]
+    decoder.decode(Base64("BmZvb4wfBmJhcg==")) shouldBe MovieChangedV2("foo", 1990, "bar")
   }
 
   "MovieChangedV3" should "encode to base64" in {
     val event = MovieChangedV3("foo", 1990, "bar")
-    val encoder = implicitly[Encoder[MovieChangedV3, String]]
-    val base64String = encoder.encode(event)
-    base64String shouldBe "BmZvb4wfBmJhcg=="
+    val encoder = implicitly[Encoder[MovieChangedV3, Base64]]
+    val base64 = encoder.encode(event)
+    base64.value shouldBe "BmZvb4wfBmJhcg=="
   }
 
   it should "decode from base64" in {
-    val decoder = implicitly[Decoder[String, MovieChangedV3]]
-    decoder.decode("BmZvb4wfBmJhcg==") shouldBe MovieChangedV3("foo", 1990, "bar")
+    val decoder = implicitly[Decoder[Base64, MovieChangedV3]]
+    decoder.decode(Base64("BmZvb4wfBmJhcg==")) shouldBe MovieChangedV3("foo", 1990, "bar")
   }
 
   "v1 to v2" should "decode with format" in {
     val decoder = implicitly[Decoder[AvroCommand, MovieChangedV2]]
     decoder.decode(AvroCommand(
-      "BmZvb4wf",
+      Base64("BmZvb4wf"),
       oldSchema =
         """
-            |{
-            |  "type" : "record",
-            |  "name" : "MovieChanged",
-            |  "version" : 1,
-            |  "namespace" : "com.github.dnvriend.serializer.avro4s",
-            |  "fields" : [
-            |   { "name" : "title", "type" : "string" },
-            |   { "name" : "year", "type" : "int" }
-            |  ]
-            |}
-          """.stripMargin.toSchema,
+        |{
+        |  "type" : "record",
+        |  "name" : "MovieChanged",
+        |  "version" : 1,
+        |  "namespace" : "foo.bar",
+        |  "fields" : [
+        |   { "name" : "title", "type" : "string" },
+        |   { "name" : "year", "type" : "int" }
+        |  ]
+        |}
+      """.stripMargin.toSchema,
       newSchema =
         """
-          |{
-          |  "type" : "record",
-          |  "name" : "MovieChanged",
-          |  "version" : 2,
-          |  "namespace" : "com.github.dnvriend.serializer.avro4s",
-          |  "fields" : [
-          |   { "name" : "title", "type" : "string" },
-          |   { "name" : "year", "type" : "int" },
-          |   { "name" : "director", "type" : "string", "default" : "unknown" }
-          |  ]
-          |}
-        """.stripMargin.toSchema
+        |{
+        |  "type" : "record",
+        |  "name" : "MovieChanged",
+        |  "version" : 2,
+        |  "namespace" : "foo.bar",
+        |  "fields" : [
+        |   { "name" : "title", "type" : "string" },
+        |   { "name" : "year", "type" : "int" },
+        |   { "name" : "director", "type" : "string", "default" : "unknown" }
+        |  ]
+        |}
+      """.stripMargin.toSchema
     )) shouldBe MovieChangedV2("foo", 1990, "unknown")
   }
 
   "v2 to v3" should "decode with format" in {
     val decoder = implicitly[Decoder[AvroCommand, MovieChangedV3]]
     decoder.decode(AvroCommand(
-      "BmZvb4wfBmJhcg==",
+      Base64("BmZvb4wfBmJhcg=="),
       oldSchema = """
         |{
         |  "type" : "record",
         |  "name" : "MovieChanged",
         |  "version" : 2,
-        |  "namespace" : "com.github.dnvriend.serializer.avro4s",
+        |  "namespace" : "foo.bar",
         |  "fields" : [
         |   { "name" : "title", "type" : "string" },
         |   { "name" : "year", "type" : "int" },
@@ -121,7 +123,7 @@ class AvroSENoOldClassesTest extends AvroTestSpec {
         |  "type" : "record",
         |  "name" : "MovieChanged",
         |  "version" : 3,
-        |  "namespace" : "com.github.dnvriend.serializer.avro4s",
+        |  "namespace" : "foo.bar",
         |  "fields" : [
         |   { "name" : "title", "type" : "string"},
         |   { "name" : "released_year", "type" : "int", "aliases" : ["year"] },
@@ -135,14 +137,14 @@ class AvroSENoOldClassesTest extends AvroTestSpec {
   "v1 to v3" should "decode with format" in {
     val decoder = implicitly[Decoder[AvroCommand, MovieChangedV3]]
     decoder.decode(AvroCommand(
-      "BmZvb4wf",
+      Base64("BmZvb4wf"),
       oldSchema =
         """
         |{
         |  "type" : "record",
         |  "name" : "MovieChanged",
         |  "version" : 1,
-        |  "namespace" : "com.github.dnvriend.serializer.avro4s",
+        |  "namespace" : "foo.bar",
         |  "fields" : [
         |   { "name" : "title", "type" : "string" },
         |   { "name" : "year", "type" : "int" }
@@ -155,7 +157,7 @@ class AvroSENoOldClassesTest extends AvroTestSpec {
         |  "type" : "record",
         |  "name" : "MovieChanged",
         |  "version" : 3,
-        |  "namespace" : "com.github.dnvriend.serializer.avro4s",
+        |  "namespace" : "foo.bar",
         |  "fields" : [
         |   { "name" : "title", "type" : "string"},
         |   { "name" : "released_year", "type" : "int", "aliases" : ["year"] },
@@ -164,5 +166,74 @@ class AvroSENoOldClassesTest extends AvroTestSpec {
         |}
       """.stripMargin.toSchema
     )) shouldBe MovieChangedV3("foo", 1990, "unknown")
+  }
+
+  "v1 to v4" should "decode with format" in {
+    val decoder = implicitly[Decoder[AvroCommand, MovieChangedV4]]
+    decoder.decode(AvroCommand(
+      Base64("BmZvb4wf"),
+      oldSchema =
+        """
+          |{
+          |  "type" : "record",
+          |  "name" : "MovieChanged",
+          |  "version" : 1,
+          |  "namespace" : "foo.bar",
+          |  "fields" : [
+          |   { "name" : "title", "type" : "string", "default" : "" },
+          |   { "name" : "year", "type" : "int", "default" : 0 }
+          |  ]
+          |}
+        """.stripMargin.toSchema,
+      newSchema =
+        """
+          |{
+          |  "type" : "record",
+          |  "name" : "MovieChanged",
+          |  "version" : 3,
+          |  "namespace" : "foo.bar",
+          |  "fields" : [
+          |   { "name" : "title", "type" : "string", "default" : "" },
+          |   { "name" : "director", "type" : "string", "default" : "" },
+          |   { "name" : "wonOscars", "type" : "int", "default" : 0 }
+          |  ]
+          |}
+        """.stripMargin.toSchema
+    )) shouldBe MovieChangedV4("foo", "", 0)
+  }
+
+  "v1 to v5" should "decode with format" in {
+    val decoder = implicitly[Decoder[AvroCommand, MovieChangedV5]]
+    decoder.decode(AvroCommand(
+      Base64("BmZvb4wf"),
+      oldSchema =
+        """
+          |{
+          |  "type" : "record",
+          |  "name" : "MovieChanged",
+          |  "version" : 1,
+          |  "namespace" : "foo.bar",
+          |  "fields" : [
+          |   { "name" : "title", "type" : "string", "default" : "" },
+          |   { "name" : "year", "type" : "int", "default" : 0 }
+          |  ]
+          |}
+        """.stripMargin.toSchema,
+      newSchema =
+        """
+          |{
+          |  "type" : "record",
+          |  "name" : "MovieChanged",
+          |  "version" : 3,
+          |  "namespace" : "foo.bar",
+          |  "fields" : [
+          |   { "name" : "title", "type" : "string", "default" : "" },
+          |   { "name" : "director", "type" : "string", "default" : "unknown" },
+          |   { "name" : "wonOscars", "type" : "int", "default" : 0 },
+          |   { "name" : "releases", "type" : { "type": "map", "values" : "int" }, "default" : {  } }
+          |  ]
+          |}
+        """.stripMargin.toSchema
+    )) shouldBe MovieChangedV5("foo", "unknown", 0, Map.empty)
   }
 }
--- src/test/scala/com/github/dnvriend/serializer/avro4s/AvroTestSpec.scala
+++ src/test/scala/com/github/dnvriend/serializer/avro4s/AvroTestSpec.scala
@@ -19,6 +19,7 @@ package com.github.dnvriend.serializer.avro4s
 import java.io.ByteArrayOutputStream
 
 import com.github.dnvriend.TestSpec
+import com.github.dnvriend.domain.Movie.{ Country, Year }
 import com.sksamuel.avro4s._
 import org.apache.avro.Schema
 import org.apache.avro.file.SeekableByteArrayInput
@@ -26,9 +27,9 @@ import org.apache.avro.generic.{ GenericDatumReader, GenericRecord }
 import org.apache.avro.io.DecoderFactory
 
 object Encoder {
-  implicit val Base64Encoder = new Encoder[Array[Byte], String] {
-    override def encode(in: Array[Byte]): String =
-      java.util.Base64.getEncoder.encodeToString(in)
+  implicit val Base64Encoder = new Encoder[Array[Byte], Base64] {
+    override def encode(in: Array[Byte]): Base64 =
+      Base64(java.util.Base64.getEncoder.encodeToString(in))
   }
 
   implicit def EventAvroEncoder[A <: Event: SchemaFor: ToRecord] = new Encoder[A, Array[Byte]] {
@@ -41,23 +42,29 @@ object Encoder {
     }
   }
 
-  implicit def EventBase64Encoder[A <: Event: SchemaFor: ToRecord] = new Encoder[A, String] {
-    override def encode(in: A): String = {
+  implicit def EventBase64Encoder[A <: Event: SchemaFor: ToRecord] = new Encoder[A, Base64] {
+    override def encode(in: A): Base64 = {
       val avro = implicitly[Encoder[A, Array[Byte]]].encode _
-      val base64 = implicitly[Encoder[Array[Byte], String]].encode _
+      val base64 = implicitly[Encoder[Array[Byte], Base64]].encode _
       (avro andThen base64)(in)
     }
   }
 }
-
+import annotation.implicitNotFound
+@implicitNotFound("No member of type class Encoder in scope for in: ${IN}, out: ${OUT}")
 trait Encoder[IN, OUT] {
   def encode(in: IN): OUT
 }
 
 object Decoder {
-  implicit val Base64Decoder = new Decoder[String, Array[Byte]] {
-    override def decode(in: String): Array[Byte] =
-      java.util.Base64.getDecoder.decode(in)
+  implicit val Base64ToByteArrayDecoder = new Decoder[Base64, Array[Byte]] {
+    override def decode(in: Base64): Array[Byte] =
+      java.util.Base64.getDecoder.decode(in.value)
+  }
+
+  implicit val Base64ToStringDecoder = new Decoder[Base64, String] {
+    override def decode(in: Base64): String =
+      new String(implicitly[Decoder[Base64, Array[Byte]]].decode(in))
   }
 
   implicit def AvroInputStreamDecoder[A <: Event] = new Decoder[AvroInputStream[A], A] {
@@ -71,9 +78,9 @@ object Decoder {
         .decode(AvroInputStream[A](in))
   }
 
-  implicit def EventBase64Decoder[A <: Event: SchemaFor: FromRecord] = new Decoder[String, A] {
-    override def decode(in: String): A = {
-      val base64 = implicitly[Decoder[String, Array[Byte]]].decode _
+  implicit def EventBase64Decoder[A <: Event: SchemaFor: FromRecord] = new Decoder[Base64, A] {
+    override def decode(in: Base64): A = {
+      val base64 = implicitly[Decoder[Base64, Array[Byte]]].decode _
       val avro = implicitly[Decoder[Array[Byte], A]].decode _
       (base64 andThen avro)(in)
     }
@@ -82,7 +89,7 @@ object Decoder {
   implicit def AvroSchemaDecoder[A <: Event: ToRecord: FromRecord: RecordFormat] = new Decoder[AvroCommand, A] {
     override def decode(in: AvroCommand): A = {
       val gdr = new GenericDatumReader[GenericRecord](in.oldSchema, in.newSchema) // (writer, reader)
-      val seek = new SeekableByteArrayInput(implicitly[Decoder[String, Array[Byte]]].decode(in.base64))
+      val seek = new SeekableByteArrayInput(implicitly[Decoder[Base64, Array[Byte]]].decode(in.base64))
       val binDecoder = DecoderFactory.get().binaryDecoder(seek, null)
       val record: GenericRecord = gdr.read(null, binDecoder)
       val format = RecordFormat[A]
@@ -91,19 +98,32 @@ object Decoder {
   }
 }
 
+import annotation.implicitNotFound
+@implicitNotFound("No member of type class Decoder in scope for in: ${IN}, out: ${OUT}")
 trait Decoder[IN, OUT] {
   def decode(in: IN): OUT
 }
 
-case class AvroCommand(base64: String, oldSchema: Schema, newSchema: Schema)
+case class Base64(value: String)
+case class AvroCommand(base64: Base64, oldSchema: Schema, newSchema: Schema)
 
 trait Event
 final case class MovieChangedV1(title: String, year: Int) extends Event
 final case class MovieChangedV2(title: String, year: Int, director: String) extends Event
 final case class MovieChangedV3(title: String, released_year: Int, director: String) extends Event
+final case class MovieChangedV4(title: String, director: String, wonOscars: Int = 0) extends Event
+final case class MovieChangedV5(title: String, director: String, wonOscars: Int = 0, releases: Map[Country, Year]) extends Event
 
 class AvroTestSpec extends TestSpec {
   implicit class StringToSchema(json: String) {
     def toSchema: Schema = new (Schema.Parser).parse(json)
+  }
+
+  implicit class ArrayOps(xs: Array[Byte]) {
+    def encode[OUT](implicit ev: Encoder[Array[Byte], OUT]): OUT = ev.encode(xs)
+  }
+
+  implicit class Base64Ops(base64: Base64) {
+    def decode[OUT](implicit ev: Decoder[Base64, OUT]): OUT = ev.decode(base64)
   }
 }
